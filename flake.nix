bfc0e6a31a309ee186704887f58c4f6fc356c30b
conao3 <conao3@gmail.com>
2026-02-22 13:26:13 +0000

    add flake

---
+++ flake.nix
@@ -0,0 +1,57 @@
+{
+  description = "rust-fetch-usage-limit development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+  };
+
+  outputs =
+    inputs@{
+      flake-parts,
+      treefmt-nix,
+      ...
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-darwin"
+      ];
+
+      perSystem =
+        {
+          system,
+          ...
+        }:
+        let
+          overlay = final: prev: {
+            rustc = prev.rustc;
+            cargo = prev.cargo;
+            clippy = prev.clippy;
+            rustfmt = prev.rustfmt;
+          };
+          pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = [ overlay ];
+          };
+          treefmtEval = treefmt-nix.lib.evalModule pkgs {
+            projectRootFile = "flake.nix";
+            programs.nixfmt.enable = true;
+            programs.rustfmt.enable = true;
+          };
+        in
+        {
+          formatter = treefmtEval.config.build.wrapper;
+
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              rustc
+              cargo
+              clippy
+              rustfmt
+            ];
+          };
+        };
+    };
+}
